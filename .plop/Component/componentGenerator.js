d38cea55e3f290df16a5c0846db80607240e558e
AlexanderShushunov <AShushunov@mail.ru>
2020-02-09 21:11:29 +0300

    Add test generator

--- .plop/Component/componentGenerator.js
+++ .plop/Component/componentGenerator.js
@@ -4,21 +4,48 @@ const { getTemplates } = require('../utils/getTemplates')
 const templates = getTemplates(path.resolve(__dirname, 'templates'))
 
 function componentGenerator (dist) {
-  return {
-    prompts: [{
-      type: 'input',
-      name: 'name',
-      message: 'component name'
-    }],
-    actions: [{
+  function makeDist (fileName) {
+    return path.resolve(dist, `{{pascalCase name}}/${fileName}`)
+  }
+
+  const prompts = [{
+    type: 'input',
+    name: 'name',
+    message: 'component name'
+  }, {
+    type: 'confirm',
+    name: 'test',
+    message: 'add jest test'
+  }]
+
+  function actions ({test}) {
+    const addBaseFiles = [{
       type: 'add',
-      path: path.resolve(dist, '{{pascalCase name}}/index.js'),
-      templateFile: templates.index
+      path: makeDist('index.js'),
+      templateFile: templates['index']
     }, {
       type: 'add',
-      path: path.resolve(dist, '{{pascalCase name}}/{{pascalCase name}}.vue'),
-      templateFile: templates.Component
+      path: makeDist('{{pascalCase name}}.vue'),
+      templateFile: templates['Component']
     }]
+
+    const addTest = {
+      type: 'add',
+      path: makeDist('{{pascalCase name}}.spec.js'),
+      templateFile: templates['Component.spec']
+    }
+
+    const res = []
+    res.push(...addBaseFiles)
+    if (test) {
+      res.push(addTest)
+    }
+    return res
+  }
+
+  return {
+    prompts,
+    actions
   }
 }
 
